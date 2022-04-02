05e5dd8b350717542c72e637091226bad510f6ff
Dương Phạm <pdtduong03@gmail.com>
2022-04-02 15:15:17 -0700

    Commit "Unit Test For Functions"

---
+++ test/KnowLedgeBackendServer.UnitTest/InMemoryDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using KnowledgeSpace.BackendServer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowLedgeBackendServer.UnitTest
+{
+    public class InMemoryDbContextFactory
+    {
+        public ApplicationDbContext GetApplicationDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                       .UseInMemoryDatabase(databaseName: "InMemoryApplicationDatabase")
+                       .Options;
+            var dbContext = new ApplicationDbContext(options);
+
+            return dbContext;
+        }
+    }
+}
