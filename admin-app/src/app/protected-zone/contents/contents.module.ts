e1e8fc742cc00b5d6d7c8a9529b3b1c94d7690a6
Dương Phạm <pdtduong03@gmail.com>
2022-04-10 18:54:52 +0700

    Commit "Create Angular Structure"

---
+++ admin-app/src/app/protected-zone/contents/contents.module.ts
@@ -0,0 +1,23 @@
+import { ContentsRoutingModule } from './contents-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesComponent } from './categories/categories.component';
+import { KnowledgeBasesComponent } from './knowledge-bases/knowledge-bases.component';
+import { CommentsComponent } from './comments/comments.component';
+import { ReportsComponent } from './reports/reports.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoriesComponent,
+    KnowledgeBasesComponent,
+    CommentsComponent,
+    ReportsComponent
+  ],
+  imports: [
+    CommonModule,
+    ContentsRoutingModule
+  ]
+})
+export class ContentsModule { }
