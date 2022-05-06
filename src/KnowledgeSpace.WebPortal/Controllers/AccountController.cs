e611b2e82207db5dd1af73c12db9d1b79a36d322
thienanga075 <pdtduong@gmail.com>
2022-05-06 22:24:01 +0700

    Mess "Integrate SSO with Identity Server"

---
+++ src/KnowledgeSpace.WebPortal/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KnowledgeSpace.WebPortal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "oidc");
+        }
+
+        public IActionResult SignOut()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = "/" }, "Cookies", "oidc");
+        }
+
+        [Authorize]
+        public ActionResult MyProfile()
+        {
+            return View();
+        }
+    }
+}
