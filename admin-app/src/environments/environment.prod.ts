c8d14dfb3fc46752cf24aad36dfd38c397b4af3b
thienanga075 <pdtduong@gmail.com>
2022-05-06 11:01:30 +0700

    Mess "Add Template"

--- admin-app/src/environments/environment.prod.ts
+++ admin-app/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://kbapi.com.vn'
+    apiUrl: 'https://kbapi.com.vn',
+    // authorityUrl: 'https://kbs.com.vn',
+    // clientId: 'angular_admin',
+    // adminUrl: 'https://kbsadmin.com.vn',
 };
